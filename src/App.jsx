47d1c8488640146fa1645b96633e76af99b4d3a9
vaijaaaaa <vaijuwalker111@gmail.com>
2025-01-09 11:27:42 +0530

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <h1>Learn about redux Toolkit</h1>
+    <>
+      <h1>Learn about redux Toolkit</h1>
+      <AddTodo />
+    </>
   )
 }
 
